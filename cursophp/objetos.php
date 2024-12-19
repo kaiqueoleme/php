3abd8d22efe1fb7b7e6a52256ed374d90a2419bb
kaiqueoleme <166898993+kaiqueoleme@users.noreply.github.com>
2024-12-18 23:23:28 -0300

    Add heranca

---
+++ cursophp/objetos.php
@@ -0,0 +1,60 @@
+<?php
+
+class Login
+{
+    private $email;
+    private $senha;
+    private $nome;
+
+    public function __construct($email, $senha, $nome)
+    {
+        $this->setNome($nome);
+        $this->setEmail($email);
+        $this->setSenha($senha);
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($n)
+    {
+        $this->nome = $n;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($e)
+    {
+        $email = filter_var($e, FILTER_SANITIZE_EMAIL);
+        $this->email = $email;
+    }
+
+    public function getSenha()
+    {
+        return $this->senha;
+    }
+
+    public function setSenha($s)
+    {
+        $this->senha = $s;
+    }
+
+    public function Logar()
+    {
+        if ($this->getEmail() == "teste@teste.com" and $this->getSenha() == "123456") {
+            echo "Logado com sucesso!";
+        } else {
+            echo "Dados Inválidos";
+        }
+    }
+}
+
+$logar = new Login("teste@teste.com", "123456", "Rodrigo Oliveira");
+$logar->Logar();
+echo "<br>";
+echo $logar->getNome();

8938531fb153ca3dcb9d06cdff50568d402b515c
KaikP <166898993+kaiqueoleme@users.noreply.github.com>
2025-04-22 01:28:31 -0300

    Correcoes

--- php2/att/Paciente.php
+++ php2/att/Paciente.php
@@ -1,62 +1,70 @@
 <?php
-    class Paciente {
-        //Atributos
-        private $nome;
-        private $rg;
-        private $cpf;
-        private $end;
-        private $prof;
+class Paciente
+{
+    //Atributos
+    private $nome;
+    private $rg;
+    private $cpf;
+    private $end;
+    private $prof;
 
-        // Método Construtor
-        public function __construct($nome, $rg, $cpf)
-        {
-            $this->nome = $nome;
-            $this->rg = $rg;
-            $this->cpf = $cpf;
-        }
-
-
-        //Getters e Setters dos atributos
-        public function getNome() {
-            return $this->nome;
-        }
-
-        public function setNome($nome) {
-            $this->nome = $nome;
-        }
-
-        public function getRg() {
-            return $this->rg;
-        }
-
-        public function setRg($rg) {
-            $this->rg = $rg;
-        }
-
-        public function getCpf() {
-            return $this->cpf;
-        }
-
-        public function setCpf($cpf) {
-            $this->cpf = $cpf;
-        }
-
-        public function getEndereco() {
-            return $this->end;
-        }
-
-        public function setEndereco($end) {
-            $this->end = $end;
-        }
-
-        public function getProfissao() {
-            return $this->prof;
-        }
-
-        public function setProfissao($prof) {
-            $this->prof = $prof;
-        }
-
+    // Método Construtor
+    public function __construct($nome, $rg, $cpf)
+    {
+        $this->nome = $nome;
+        $this->rg = $rg;
+        $this->cpf = $cpf;
     }
-?>
 
+
+    //Getters e Setters dos atributos
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function getRg()
+    {
+        return $this->rg;
+    }
+
+    public function setRg($rg)
+    {
+        $this->rg = $rg;
+    }
+
+    public function getCpf()
+    {
+        return $this->cpf;
+    }
+
+    public function setCpf($cpf)
+    {
+        $this->cpf = $cpf;
+    }
+
+    public function getEndereco()
+    {
+        return $this->end;
+    }
+
+    public function setEndereco($end)
+    {
+        $this->end = $end;
+    }
+
+    public function getProfissao()
+    {
+        return $this->prof;
+    }
+
+    public function setProfissao($prof)
+    {
+        $this->prof = $prof;
+    }
+}
--- php2/att/index.php
+++ php2/att/index.php
@@ -1,24 +1,31 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agenda 09 - DSI III</title>
 </head>
+
 <body>
     <?php
+    // Includes
     include_once 'Paciente.php';
 
+    // Criando objetos
     $paciente = new Paciente("Pedro", "161752421", "22136732846");
     $paciente->setEndereco("Avenida Afonso Pena, Bairro Boa Viagem, Belo Horizonte, MG");
     $paciente->setProfissao("Farmacêutico");
 
-    echo "Nome: ". $paciente->getNome(). "<br>";
-    echo "RG: ". $paciente->getRg(). "<br>";
-    echo "CPF: ". $paciente->getCpf(). "<br>";
-    echo "Endereço: ". $paciente->getEndereco(). "<br>";
-    echo "Profissão: ". $paciente->getProfissao(). "<br>";
-    
+    // Saida de dados
+    echo "Nome: " . $paciente->getNome() . "<br>";
+    echo "RG: " . $paciente->getRg() . "<br>";
+    echo "CPF: " . $paciente->getCpf() . "<br>";
+    echo "Endereço: " . $paciente->getEndereco() . "<br>";
+    echo "Profissão: " . $paciente->getProfissao() . "<br>";
+
     ?>
 </body>
+
 </html>
+

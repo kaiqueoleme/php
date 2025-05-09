5adfb6ee19628f06139ee7785566eeb66288d648
KaikP <166898993+kaiqueoleme@users.noreply.github.com>
2025-05-08 23:21:17 -0300

    php

--- dsi/View/principal.php
+++ dsi/View/principal.php
@@ -45,7 +45,7 @@
     </nav>
     <header class="w3-container w3-padding-32 w3-center " id="home">
         <h1>
-            <img src="../Img/Enlatados.png" alt="Logo" class="w3-image" width="50%">
+            <img src="../Img/Enlatados.png" alt="Logo" class="w3-image" width="30%">
             </br>
         </h1>
         <a class="w3-text-cyan" href="http://www.freepik.com">Designed by brgfx / Freepik</a>
@@ -53,9 +53,9 @@
         <h1 class="w3-text-cyan">SISTEMA DE CURRICULOS</h1>
     </header>
     <div class="w3-padding-large" id="main">
-        <div class="w3-padding-128 w3-content w3-text-grey" id="dPessoais">
+        <div class="w3-padding-128 w3-content w3-text-grey" id="dPessoais" style="">
             <h2 class="w3-text-cyan">Dados Pessoais</h2>
-            <form action="" method="post" class=" w3-row w3-light-grey w3-text-blue w3-margin" style="width:70%;">
+            <form action="" method="post" class=" w3-row w3-light-grey w3-text-blue w3-margin" style="width:70%; ">
                 <input class="w3-input w3-border w3-round-large" name="txtID" type="hidden" value="">
                 <div class="w3-row w3-section">
                     <div class="w3-col" style="width:11%;">
@@ -65,10 +65,28 @@
                         <input class="w3-input w3-border w3-round-large" name="txtNome" type="text" placeholder="NomeCompleto" value="">
                     </div>
                 </div>
-                
+
                 <div class="w3-row w3-section">
                     <div class="w3-col" style="width:11%;">
-                        <i class="w3-xxlarge fa fa-envelope"></i>
+                        <i class="w3-xxlarge fa-regular fa-calendar-days"></i>
+                    </div>
+                    <div class="w3-rest">
+                        <input class="w3-input w3-border w3-round-large" name="txtNome" type="date" placeholder="dd/mm/yyyy" value="">
+                    </div>
+                </div>
+
+                <div class="w3-row w3-section">
+                    <div class="w3-col" style="width:11%;">
+                        <i class=" w3-xxlarge fa-solid fa-id-card"></i>
+                    </div>
+                    <div class="w3-rest">
+                        <input class="w3-input w3-border w3-round-large" name="txtNome" type="text" placeholder="000.000.000-00" value="">
+                    </div>
+                </div>
+
+                <div class="w3-row w3-section">
+                    <div class="w3-col" style="width:11%;">
+                        <i class="w3-xxlarge fa-envelope fa-regular"></i>
                     </div>
                     <div class="w3-rest">
                         <input class="w3-input w3-border w3-round-large" name="txtNome" type="text" placeholder="NomeCompleto" value="">
@@ -76,20 +94,8 @@
                 </div>
 
                 <div class="w3-row w3-section">
-                    <div class="w3-col" style="width:11%;">
-                        <i class="w3-xxlarge fa fa-user"></i>
-                    </div>
-                    <div class="w3-rest">
-                        <input class="w3-input w3-border w3-round-large" name="txtNome" type="text" placeholder="NomeCompleto" value="">
-                    </div>
-                </div>
-
-                <div class="w3-row w3-section">
-                    <div class="w3-col" style="width:11%;">
-                        <i class="w3-xxlarge fa fa-user"></i>
-                    </div>
-                    <div class="w3-rest">
-                        <input class="w3-input w3-border w3-round-large" name="txtNome" type="text" placeholder="NomeCompleto" value="">
+                    <div class="w3-center" style="">
+                        <button name="btnAtualizar" class="w3-button w3-block w3-margin w3-blue w3-cell w3-round-large" style="width: 90%;">Atualizar</button>
                     </div>
                 </div>
 
@@ -97,6 +103,66 @@
             </form>
         </div>
     </div>
+
+    <div class="w3-padding-128 w3-content w3-text-grey" id="formacao">
+        <h2 class="w3-text-cyan">Formação</h2>
+        <form action="" method="post" class=" w3-row w3-light-grey w3-text-blue w3-margin" style="width:70%;">
+            <div class="w3-row w3-center">
+                <div class="w3-col" style="width:50%;">
+                    Data Inicial
+                </div>
+                <div class="w3-res" style="">
+                    Data Final
+                </div>
+            </div>
+            <div class="w3-row w3-section">
+                <div class="w3-row w3-section w3-col" style="width:45%;">
+                    <div class="w3-col" style="width:15%;">
+                        <i class="w3-xxlarge fa fa-calendar"></i>
+                    </div>
+                    <div class="w3-rest">
+                        <input class="w3-input w3-border w3-round-large" name="txtInicioFA" type="date" placeholder="">
+                    </div>
+                </div>
+                <div class="w3-row w3-section w3-rest" style="">
+                    <div class="w3-col w3-margin-left" style="width:13%;">
+                        <i class="w3-xxlarge fa fa-calendar"></i>
+                    </div>
+                    <div class="w3-rest">
+                        <input class="w3-input w3-border w3-round-large" name="txtFimFA" type="date" placeholder="">
+                    </div>
+                </div>
+                <div>
+                    <div class="w3-row w3-section">
+                        <div class="w3-col" style="width:7%;">
+                            <i class="w3-xxlarge fa fa-align-justify"></i>
+                        </div>
+                        <div class="w3-rest">
+                            <input class="w3-input w3-border w3-round-large" name="txtDescFA" type="text" placeholder="Descrição: Ex.: Técnico em Desenvolvimento de Sistemas - Centro Paula Souza">
+                        </div>
+                    </div>
+                    <div class="w3-row w3-section">
+                        <div class="w3-center" style="">
+                            <button name="btnAddFormacao" class="w3-button w3-block w3-blue w3-cell w3-round-large" style="width: 20%;">
+                                <i class="w3-xxlarge fa fa-user-plus"></i>
+                            </button>
+                        </div>
+                    </div>
+        </form>
+        <div class="w3-container">
+            <table class="w3-table-all w3-centered">
+                <thead>
+                    <tr class="w3-center w3-blue">
+                        <th>Início</th>
+                        <th>Fim</th>
+                        <th>Descrição</th>
+                        <th>Apagar</th>
+                    </tr>
+                    <thead>
+            </table>
+        </div>
+    </div>
+
 
     <script src="https://kit.fontawesome.com/e5972cb3d1.js" crossorigin="anonymous"></script>
 </body>
